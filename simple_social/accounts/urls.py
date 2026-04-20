87de5173ad93cd0b93f8dae90d3edd76ef58749f
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-20 16:18:14 +0300

    added 2 new apps Groups and posts

--- simple_social/accounts/urls.py
+++ simple_social/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from accounts import views
 
@@ -6,18 +6,6 @@ app_name = "accounts"
 
 urlpatterns = [
     # ... your other urls
-    path(
-        "password/",
-        auth_views.PasswordChangeView.as_view(
-            template_name="accounts/change_password.html"
-        ),
-        name="password_change",
-    ),
-    path(
-        "password/done/",
-        auth_views.PasswordChangeDoneView.as_view(),
-        name="password_change_done",
-    ),
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
