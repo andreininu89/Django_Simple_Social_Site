9df4e5ce217a09a7dee26c5f51917a330f7924f8
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-21 16:01:54 +0300

    populating html files for groups

--- simple_social/groups/views.py
+++ simple_social/groups/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView, ListView
+from groups.models import Group
 
 # Create your views here.
+
+
+class GroupCreateView(LoginRequiredMixin, CreateView):
+    fields = ["name", "description"]
+    model = Group
+
+
+class SingleGroupDetailView(DetailView):
+    model = Group
+
+
+class SingleGroupListView(ListView):
+    model = Group
